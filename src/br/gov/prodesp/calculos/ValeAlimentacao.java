5963768683ca0a16ff497da3a401b2c66df0f358
Ricardo Miranda <rcmdesenvolvimento@gmail.com>
2022-04-03 21:45:17 -0300

    Reajuste no valor de desconto do Vale Alimentação. Novo valor : 19,68.

--- src/br/gov/prodesp/calculos/ValeAlimentacao.java
+++ src/br/gov/prodesp/calculos/ValeAlimentacao.java
@@ -3,25 +3,23 @@ package br.gov.prodesp.calculos;
 public class ValeAlimentacao {
 
 	public static void main(String[] args) {
-		
+
 		/*
-		 * Auto  : Ricardo Cardoso miranda
-		 * Data  : 03/04/2022
+		 * Auto : Ricardo Cardoso miranda 
+		 * Data : 03/04/2022
 		 */
-		
-		
+
 		double salario = 0;
 		double desconto = 0;
 		double novoSalario = 0;
-		
-		salario     = 15600.20;
-		desconto    = 15.68;
-		novoSalario = salario-desconto;
-		
-		
-		System.out.println("Salário        "+salario);
-		System.out.println("Valor do VA    "+desconto);
-		System.out.println("Novo Salários  "+novoSalario);
+
+		salario = 15600.20;
+		desconto = 19.68;
+		novoSalario = salario - desconto;
+
+		System.out.println("Salário        " + salario);
+		System.out.println("Valor do VA    " + desconto);
+		System.out.println("Novo Salários  " + novoSalario);
 
 	}
 
